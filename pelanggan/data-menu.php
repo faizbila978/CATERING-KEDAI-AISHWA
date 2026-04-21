ccaeb7eb9c8f6270ea5791670ed2a8de8dc56b30
faizbila978 <faizbilah978@gmail.com>
2026-04-21 09:59:33 +0700

    restore deleted files

---
+++ pelanggan/data-menu.php
@@ -0,0 +1,61 @@
+<?php
+// 1. Data Menu (Gunakan Angka Murni untuk harga)
+$menus = [
+    [
+        "id" => 1,
+        "nama" => "Nasi Kotak Ayam Bakar",
+        "deskripsi" => "Nasi, Ayam Bakar, Lalapan, Sambal.",
+        "harga" => 20000,
+        "gambar" => "ayambakar.png",
+        "kategori" => "nasi"
+    ],
+    [
+        "id" => 2,
+        "nama" => "Nasi Ayam Krispy",
+        "deskripsi" => "Nasi, Ayam krispy, Lalapan, Sambal.",
+        "harga" => 45000,
+        "gambar" => "ayamkrispy.png",
+        "kategori" => "nasi"
+    ],
+    [
+        "id" => 3,
+        "nama" => "Beef Teriyaki Bowl",
+        "deskripsi" => "Daging sapi slice dengan saus premium.",
+        "harga" => 45000,
+        "gambar" => "kentang.png",
+        "kategori" => "ricebowl"
+    ],
+    [
+        "id" => 4,
+        "nama" => "nasi tumpeng nusantara",
+        "deskripsi" => "Nasi kuning, Orek Tempe, Ayam, urap, smabal.",
+        "harga" => 30000,
+        "gambar" => "tumpeng.png",
+        "kategori" => "nasi"
+    ],
+    [
+        "id" => 5,
+        "nama" => "Chicken Katsu Bowl",
+        "deskripsi" => "Ayam katsu renyah dengan salad segar.",
+        "harga" => 30000,
+        "gambar" => "img/chicken-bowl.jpg",
+        "kategori" => "ricebowl"
+    ],
+    [
+        "id" => 6,
+        "nama" => "Box Jajanan Pasar",
+        "deskripsi" => "Isi 3 macam kue tradisional pilihan.",
+        "harga" => 15000,
+        "gambar" => "img/snack.jpg",
+        "kategori" => "snack"
+    ]
+];
+
+// Fungsi bantuan untuk memfilter menu berdasarkan kategori
+function filterMenu($menu_list, $category) {
+    if ($category == 'all') return $menu_list;
+    return array_filter($menu_list, function($item) use ($category) {
+        return $item['kategori'] == $category;
+    });
+}
+?>
